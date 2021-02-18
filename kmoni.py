57d71ffb35563e014e391f3c3eaff279f9c2d8dd
shioken <kasehonitan@gmail.com>
2021-02-18 20:08:31 +0900

    Use thread

--- kmoni.py
+++ kmoni.py
@@ -1,12 +1,69 @@
 #!/usr/bin/env python3
 from selenium import webdriver
-import time
+import asyncio
+import time, threading
 import re
 import os
 import make_gif
 import multiprocessing
 
+
+e_count = 0
+count = 0
+earthquake_now = False
+save_dir = ''
+driver = ""
+
+def monitoring():
+    global earthquake_now
+    global count
+    global driver
+    global save_dir
+    global e_count
+
+    message_time = driver.find_element_by_id('map-message-time').text
+    message_num = driver.find_element_by_id('map-message-num').text
+    message_area = driver.find_element_by_id('map-message-area').text
+    mag = driver.find_element_by_id('map-message-mag-value').text
+    depth = driver.find_element_by_id('map-message-depth-value').text
+    sindo = driver.find_element_by_id('map-message-sindo-value').text
+    alert = driver.find_element_by_id('map-message-alert-value').text
+
+    if earthquake_now:
+        print(f"{message_time},{message_num}, {message_area}, {mag}, {depth}, {sindo}, {alert}")
+        if len(message_time) == 0:
+            print("地震終了")
+            earthquake_now = False
+            count = 0
+
+            p = multiprocessing.Process(target=make_gif.make_gif, args=(save_dir,))
+            p.start()
+        else:
+            count += 1
+            cz = "{0:05d}".format(count)
+            driver.save_screenshot(f"capture/{save_dir}/eq_{e_count}_{cz}.png")
+
+    else:
+        if len(message_time) > 0:
+            print("地震発生")
+            print(f"{message_time},{message_num}, {message_area}, {mag}, {depth}, {sindo}, {alert}")
+
+            save_dir = re.sub(r"\D", "", message_time)
+            print(f"save_dir:{save_dir}")
+            os.makedirs(f"capture/{save_dir}", exist_ok=True)
+
+            earthquake_now = True
+
+            e_count += 1
+            count = 1
+            cz = "{0:05d}".format(count)
+            driver.save_screenshot(f"capture/{save_dir}/eq_{e_count}_{cz}.png")
+
+
+
 def main_func():
+    global driver
+
     options = webdriver.ChromeOptions()
     options.add_argument('--headless')
     options.add_argument('--window-size=365,720')
@@ -27,51 +84,14 @@ def main_func():
 
     print("start caturing")
 
-    e_count = 0
-    count = 0
-    earthquake_now = False
-    save_dir = ''
-
+    base_time = time.time()
+    next_time = 0
     while True:
-        message_time = driver.find_element_by_id('map-message-time').text
-        message_num = driver.find_element_by_id('map-message-num').text
-        message_area = driver.find_element_by_id('map-message-area').text
-        mag = driver.find_element_by_id('map-message-mag-value').text
-        depth = driver.find_element_by_id('map-message-depth-value').text
-        sindo = driver.find_element_by_id('map-message-sindo-value').text
-        alert = driver.find_element_by_id('map-message-alert-value').text
+        t = threading.Thread(target=monitoring)
+        t.start()
 
-        if earthquake_now:
-            print(f"{message_time},{message_num}, {message_area}, {mag}, {depth}, {sindo}, {alert}")
-            if len(message_time) == 0:
-                print("地震終了")
-                earthquake_now = False
-                count = 0
-
-                p = multiprocessing.Process(target=make_gif.make_gif, args=(save_dir,))
-                p.start()
-            else:
-                count += 1
-                cz = "{0:05d}".format(count)
-                driver.save_screenshot(f"capture/{save_dir}/eq_{e_count}_{cz}.png")
-
-        else:
-            if len(message_time) > 0:
-                print("地震発生")
-                print(f"{message_time},{message_num}, {message_area}, {mag}, {depth}, {sindo}, {alert}")
-
-                save_dir = re.sub(r"\D", "", message_time)
-                print(f"save_dir:{save_dir}")
-                os.makedirs(f"capture/{save_dir}", exist_ok=True)
-
-                earthquake_now = True
-
-                e_count += 1
-                count = 1
-                cz = "{0:05d}".format(count)
-                driver.save_screenshot(f"capture/{save_dir}/eq_{e_count}_{cz}.png")
-
-        time.sleep(1)
+        next_time = ((base_time - time.time()) % 1) or 1
+        time.sleep(next_time)
 
     driver.quit()
 
---
+++ test.py
@@ -0,0 +1,17 @@
+import time, threading
+
+def worker():
+    print(time.time())
+
+def schedule(interval, f, wait=True):
+    base_time = time.time()
+    next_time = 0
+    while True:
+        t = threading.Thread(target=f)
+        t.start()
+        if wait:
+            t.join()
+        next_time = ((base_time - time.time()) % interval) or interval
+        time.sleep(next_time)
+
+schedule(5, worker, False)
