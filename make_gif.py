33a774ba71f4044bea1422e832cc4c4b6cdebf0b
shioken <kasehonitan@gmail.com>
2021-03-02 19:01:09 +0900

    gifファイルの最適化処理

--- make_gif.py
+++ make_gif.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from PIL import Image
+from pygifsicle import gifsicle
 import glob
 import re
 import os
@@ -14,8 +15,11 @@ def make_gif(path):
     files = sorted(glob.glob(f'capture/{path}/*.png'), key=os.path.getctime)
     images = list(map(lambda file: Image.open(file).crop((0, 108, 365, 605)), files))
 
+    gif_file = f"output/{path}.gif"
     ring_images.extend(images)
-    ring_images[0].save(f"output/{path}.gif", save_all=True, append_images=ring_images[1:], duration=200, loop=1, optimize=True)
+    ring_images[0].save(gif_file, save_all=True, append_images=ring_images[1:], duration=200, loop=1, optimize=True)
+
+    gifsicle(sources=gif_file, optimize=False, colors=256, options=["-O3"])
 
     print("save gif: done")
 
